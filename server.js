467ec09945b36d090713b0c2c7b029e6d56bc0ee
Oscar Robinson <oscar.w.robinson@gmail.com>
2016-04-13 13:50:22 +0100

    added missing dependencies and fixed bug where temp dir not present

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 PROJECT_ROOT = (__dirname + '/')
 
+var fs = require("fs")
+
 PARSERS = [
     "ubx"
 ]
@@ -13,6 +15,10 @@ var express = require('express')
 var app = express()
 app.use(require('./auth-middleware'))
 app.use(require('./controllers'))
+
+if (!fs.existsSync("./temp")){
+    fs.mkdirSync("./temp");
+}
 
 app.listen(3000, function() {
     console.log("Server Listening on", 3000)
